93c017800cd1b227e0296d2d12e3f7a52289d420
LidianeMara <lidianemara93@gmail.com>
2019-02-06 23:31:56 -0200

    create a new layout and button to next page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage{
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor(
+  private router: Router,
+  ) {
+}
+gotoTabsPage(){
+  this.router.navigateByUrl('tabs');
+  } 
 }
